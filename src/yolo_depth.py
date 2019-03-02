95efe3733c0c108a6b27544667f5dc48a905ff70
James S <sohn21c@hotmail.com>
2019-03-02 15:55:16 -0600

    yolo with depth information structure created

---
+++ src/yolo_depth.py
@@ -0,0 +1,99 @@
+#!usr/bin/env python3
+
+"""
+yolo_depth.py
+Author: James Sohn
+"""
+
+# importing the necessary libraries
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+import argparse
+import imutils
+import time
+import matplotlib.pyplot as plt 
+import os
+
+# global variable declarations
+
+# class definitions
+
+# function definitions
+
+# main 
+
+
+# initiating the depth camera
+# Configure depth and color streams
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+# Start streaming
+pipeline.start(config)
+time.sleep(2)
+print ("[INFO] Camera streaming started...")
+
+# argument parsing for weight and model
+ap = argparse.ArgumentParser()
+# ap.add_argument("-i", "--input", required=True, help="path to input video")
+# ap.add_argument("-o", "--output", required=True, help="path to output video")
+ap.add_argument("-y", "--yolo", required=True, help="base path to YOLO dir")
+ap.add_argument("-c", "--confidence", type=float, default=0.5, help="minimum probability to filter weak detections")
+ap.add_argument("-t", "--threshold", type=float, default=0.3, help="threshold when applying non-maxima suppresion")
+args = vars(ap.parse_args())
+
+# load the COCO class labels YOLO model was trained on
+labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
+
+# initialize a list of colors represent each possible class label
+np.random.seed(42)
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
+
+# derive the paths to the YOLO weights and model configuration
+weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
+configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
+
+# load YOLO object detector trained on COCO dataset (80 classes) and determine only the *output* layer names that we need from YOLO
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+print("[INFO] YOLO model loaded")
+
+# try:
+while True:
+    
+    # Wait for a coherent pair of frames: depth and color
+    frames = pipeline.wait_for_frames()
+    depth_frame = frames.get_depth_frame()
+    color_frame = frames.get_color_frame()
+    if not depth_frame or not color_frame:
+        continue
+
+    # Convert images to numpy arrays
+    depth_image = np.asanyarray(depth_frame.get_data())
+    color_image = np.asanyarray(color_frame.get_data())
+
+    # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+    # Stack both images horizontally
+    images = np.hstack((color_image, depth_colormap))
+
+    # Show images
+    cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+    cv2.imshow('RealSense', images)
+    key = cv2.waitKey(1)
+
+    # kill the windown when 'q' is pressed
+    if key == ord("q"):
+        break
+
+# finally:
+
+    # Stop streamin
+pipeline.stop()
