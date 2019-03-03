fd4a9d3a34acf98d0f134e484d8193f0ef6a18be
James S <sohn21c@hotmail.com>
2019-03-02 22:24:56 -0600

    clean up and comments

--- src/yolo_depth.py
+++ src/yolo_depth.py
@@ -23,66 +23,70 @@ import os
 
 # main 
 
-
 # initiating the depth camera
-# Configure depth and color streams
 pipeline = rs.pipeline()
 config = rs.config()
+# configure depth and color stream
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
-# Start streaming
+# start streaming D435 RGB-D camera
 pipeline.start(config)
+# pause for 2 seconds for warmup
 time.sleep(2)
 print ("[INFO] Camera streaming started...")
 
 # argument parsing for weight and model
+# default confidence for detection is set to 0,5 and threshold for detection is defaulted to be 0.3. Both can be reconfigured at runtime
 ap = argparse.ArgumentParser()
-# ap.add_argument("-i", "--input", required=True, help="path to input video")
-# ap.add_argument("-o", "--output", required=True, help="path to output video")
 ap.add_argument("-y", "--yolo", required=True, help="base path to YOLO dir")
 ap.add_argument("-c", "--confidence", type=float, default=0.5, help="minimum probability to filter weak detections")
 ap.add_argument("-t", "--threshold", type=float, default=0.3, help="threshold when applying non-maxima suppresion")
 args = vars(ap.parse_args())
 
-# load the COCO class labels YOLO model was trained on
+# load the label of trained model from MS-COCO data set.
 labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
 LABELS = open(labelsPath).read().strip().split("\n")
+copyLABELS = LABELS.copy()
+copyLABELS.sort()
+print ("[INFO] Object categories model is trained on: \n", copyLABELS)
 
-# initialize a list of colors represent each possible class label
+# use numpy random seed to generate same set of colors for labels
 np.random.seed(42)
 COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
 
-# derive the paths to the YOLO weights and model configuration
+# pahts to the pretrained YOLO model weights and configuration
 weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
 configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
 
-# load YOLO object detector trained on COCO dataset (80 classes) and determine only the *output* layer names that we need from YOLO
+# load pretrained YOLO model on MS-COCO data and feed into opencv DNN module
 print("[INFO] loading YOLO from disk...")
 net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
 ln = net.getLayerNames()
 ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 print("[INFO] YOLO model loaded")
 
-# try:
+# process the frame and perform detection
 while True:
     
+    # time step for FPS calculation
     start = time.time()
-    # Wait for a coherent pair of frames: depth and color
+
+    # wait for depth and image frames
     frames = pipeline.wait_for_frames()
     depth_frame = frames.get_depth_frame()
     color_frame = frames.get_color_frame()
     if not depth_frame or not color_frame:
         continue
 
-    # Convert images to numpy arrays
+    # convert images to numpy arrays
     depth_image = np.asanyarray(depth_frame.get_data())
     color_image = np.asanyarray(color_frame.get_data())
 
-    # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+    # apply colormap on depth image (image must be converted to 8-bit per pixel first)
     depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
 
-    # convert the frame into blob
+    # convert the frame into blob for YOLO
     (h, w) = color_image.shape[:2]
     blob = cv2.dnn.blobFromImage(color_image, 1 / 255.0, (416,416), swapRB=True, crop=False)
 
@@ -118,8 +122,10 @@ while True:
 
     # ensure at least one detection exists
     if len(idxs) > 0:
+
         # loop over the indexes we are keeping
         for i in idxs.flatten():
+
             # extract the bounding box coordinates
             (x, y) = (boxes[i][0], boxes[i][1])
             (w, h) = (boxes[i][2], boxes[i][3])
@@ -141,22 +147,20 @@ while True:
             depth_meter = "{:.2f}m".format(depth/1000)
             cv2.putText(color_image, depth_meter, (centerX, centerY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
-    # Stack both images horizontally
+    # stack both images horizontally
     images = np.hstack((color_image, depth_colormap))
 
-    # Show images
+    # show images
     cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
     cv2.imshow('RealSense', images)
+    
+    # press 'q' to kill the window
     key = cv2.waitKey(1)
-
-    end = time.time()
-    print ("Video frame rate is {:.2f}".format(1/(end - start)))
-
-    # kill the windown when 'q' is pressed
     if key == ord("q"):
         break
 
-# finally:
+    # time step for FPS calculation    
+    end = time.time()
+    print ("Video frame rate is {:.2f}".format(1/(end - start)))
 
-    # Stop streamin
 pipeline.stop()
